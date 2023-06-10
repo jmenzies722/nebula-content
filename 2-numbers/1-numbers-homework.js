58caab95571ac7a1037d2f1665a819d53160c481
Jmenzies722 <Jmenzies722@gmail.com>
2023-06-10 16:04:28 -0400

    homework

---
+++ 2-numbers/1-numbers-homework.js
@@ -0,0 +1,41 @@
+//Joshua Menzies Hw 1-number-homework
+// Remember to console.log() to debug your code and check your solutions!
+// Declare a variable and assign it to a numeric value
+// Half that value and re-assign it to the previous variable 
+let numVal = 100
+numVal = numVal/2
+console.log(numVal)
+// Create two negative numbers, assign them to variables and subtract one from the other together 
+const negOne = -5 
+const negTwo = -10 
+console.log(negTwo-negOne)
+// Create three numbers, assign them to variables and multiply them together 
+let mulltiOne = 3 
+let multiTwo = 6
+let multiThree = 9 
+console.log(mulltiOne*multiTwo*multiThree)
+// Create two even numbers, assign them to variables and divide one from the other 
+let evenOne = 2 
+let evenTwo = 4
+console.log(evenOne/evenTwo)
+// Create two odd numbers, assign them to variables and find the remainder of dividing one by the other 
+let oddOne = 1
+let oddTwo = 3 
+console.log(oddOne%oddTwo)
+// find the minimum value in a set of numbers: 10,20,30,40,50 
+console.log(Math.min(10,20,30,40,50))
+// find the maximum value in a set of numbers: -1,-2,-3,-4,-5 
+console.log(Math.max(-1,-2,-3,-4,-5))
+// Create a variable and find it's remainder, if divided by 2
+let valRemain = 10 
+console.log(valRemain%2)
+// find the square root of 144 
+console.log(Math.sqrt(144))
+// find the value of 3 to the power of 7 
+console.log(Math.pow(3,7))
+// find the value of 73.2 rounded 
+console.log(Math.round(73.2))
+// find the rounded-down value of 9.9 
+console.log(Math.floor(9.9))
+// find the absolute value of -1231
+console.log(Math.abs(-1231))
