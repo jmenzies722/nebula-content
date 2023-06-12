717cdcd57ef777510531eb75383ed749a6b6832a
Jmenzies722 <Jmenzies722@gmail.com>
2023-06-12 12:41:36 -0400

    update

--- 3-strings/1-string.assignment.js
+++ 3-strings/1-string.assignment.js
@@ -1,11 +1,12 @@
 
 // 1. Concatenate "hello" with "goodbye"
- const string1 = "hello"
- const string2  = "goodbye"
-
+const string1 = "hello"
+const string2  = "goodbye "
+console.log(string1 + " " + string2)
 // 2. Save the substring "Friend" from the string "Hello Friend"
 const greeting = "Hello Friend"
-
+let save = greeting.substring(6, 12)
+console.log(save)
 // 3. Using 5 or more of the following words, create a template literal that makes a legible sentence.
 //    Do not create your own words, use the existing ones below!
 const word1 = "hello"; 
@@ -24,7 +25,7 @@ const word13 = "eat";
 const word14 = "cookie"; 
 const word15 = "favorite";
 const word16 = "makeup"; 
-const word17 = "I"; 
+const word17 = "I";
 const word18 = "to"; 
 const word19 = "my";
 const word20 = "is";
@@ -32,24 +33,33 @@ const word21 = "and";
 const word22 = "also";
 const word23 = "like"; 
 
+let fullSentence = `${word1} ${word19} name ${word20} Josh ${word21} I ${word23} chocolate chip ${word14}s` //added the s at the end 
+console.log(fullSentence)
 // 4. Given the string of your full name get your initials.
-
-
+let fullName = "Joshua Menzies"
+let intials = fullName[0] + fullName[7]
+console.log(intials)
 // 5. Get the 5th character from the following string: 'Hello World'
 const greeting1 = 'Hello World'
-
+console.log(greeting1[4]) 
 // 6. Find the index of the dash from 'Coding-Time!'
 const coding = 'Coding-Time!'
-
+console.log(coding.indexOf("-"))
 // 7. Write code to grab the final letter from a any string.
 const string = "string"
-
+let lastLetter = string.slice(string.indexOf("g"))
+console.log(lastLetter)
 // 8. Write code to grab the first 3 letters from a string. 
-
-
+let str = "string"
+let firstThree = str.substring(0, 3) //subtring method
+console.log(firstThree)
 // 9. Turn the following string into an array of words: 'This-is-a-sentence';
 const sentence1 = 'This-is-a-sentence'
-
+let arrayOfWords = sentence1.split("-")
+console.log(arrayOfWords)
 // 10. Make the string of 12 from the following two numbers:
 let one = 1;
 let two = 2;
+let makeStr = String(one) + String(two)
+console.log(makeStr)
+
