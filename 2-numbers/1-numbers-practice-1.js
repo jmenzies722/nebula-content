0ea63c3b84a564253a80adeeda2a5bfb7c3aaec7
Jmenzies722 <Jmenzies722@gmail.com>
2023-06-24 04:52:32 -0400

    Add number JS files

---
+++ 2-numbers/1-numbers-practice-1.js
@@ -0,0 +1,35 @@
+// Declare a variable and assign it to a numeric value
+// Double that value and re-assign it to the previous variable
+let number = 5; 
+number = number * 2; 
+console.log(number)
+// Create two numbers, assign them to variables and add them together
+ let numOne = 10;
+ let numTwo = 50; 
+ let result = (numOne + numTwo);
+ console.log(result)
+// Create three numbers, assign them to variables and subtract the smaller two from the largest
+let number1 = 10;
+let number2 = 5;
+let number3 = 7;
+let largest = Math.max(number1, number2, number3);
+let sumOfSmallerTwo = number1 + number2 + number3 - largest;
+console.log(sumOfSmallerTwo); // Output: 12
+
+// Create two even numbers, assign them to variables and divide one from the other
+let even = 2
+let evenTwo = 10 
+let evenRes = (evenTwo/even) 
+console.log(evenRes)
+// Create two odd numbers, assign them to variables and multiply one from the other
+let odd = 3
+let oddOne = 11 
+let oddRes = (odd*oddOne) 
+console.log(oddRes)
+// Create a variable and log it's remainder, if divided by 3, to the console 
+let remainder = 3
+console.log(remainder%3)
+// Answer in plain text, what is modulo especially useful for?
+
+// This is used to find the remainder of an expression 
+// for example 3/3 = 1 and 3%3 is remainder 0
---
+++ 2-numbers/1-numbers-practice-2.js
@@ -0,0 +1,20 @@
+// For these questions use the Math methods that JavaScript includes
+// All Math methods: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+// find the square root of 25
+console.log(Math.sqrt(25))
+// find the value of 3 to the power of 5
+console.log(Math.pow(3,5))
+// find the value of 7.4 rounded
+console.log(Math.round(7.4))
+// find the rounded-up value of 7.4
+console.log(Math.ceil(7.4))
+// find the absolute value of -13
+console.log(Math.abs(-13))
+// find the minimum value in a set of numbers: 12,13,14,15,2,-1
+console.log(Math.min(12,13,14,15,2,-1))
+// find the maximum value in a set of numbers: 12,13,14,15,2,-1
+console.log(Math.max(12,13,14,15,2,-1))
+// create a random number 
+console.log(Math.random())
+// Create a random number between 0-10
+console.log(Math.random()*11)
