138bcc48fe4761ad303f5a7d45941317852bc293
Jmenzies722 <Jmenzies722@gmail.com>
2023-07-24 16:55:01 -0400

    updated css

--- 11-dom-manipulation/calculator/calc.js
+++ 11-dom-manipulation/calculator/calc.js
@@ -1,22 +1,69 @@
-let currentResult = "";
+let calculator = {
+  // This variable stores the current result of the calculator
+  currentResult: "",
 
-function appendNumber(number) {
-  currentResult += number;
-  document.getElementById("result").value = currentResult;
+  // This function appends a number to the current result
+  appendNumber(number) {
+    // Concatenate the new number to the current result
+    this.currentResult += number;
+    // Update the 'result' element in the HTML with the new current result
+    document.getElementById("result").value = this.currentResult;
+  },
+
+  // This function appends an operator to the current result
+  appendOperator(operator) {
+    // Concatenate the new operator to the current result
+    this.currentResult += operator;
+    // Update the 'result' element in the HTML with the new current result
+    document.getElementById("result").value = this.currentResult;
+  },
+
+  // This function calculates the result of the current result
+  calculateResult() {
+    // Perform the calculation and store the result
+    let result = this.performCalculation(this.currentResult);
+    // Update the 'result' element in the HTML with the result
+    document.getElementById("result").value = result;
+    // Store the result as the new current result
+    this.currentResult = result.toString();
+  },
+
+  // This function clears the current result
+  clearResult() {
+    // Reset the current result to an empty string
+    this.currentResult = "";
+    // Update the 'result' element in the HTML with the new current result
+    document.getElementById("result").value = "";
+  },
+
+  // This function performs a calculation based on an expression string
+  performCalculation(expression) {
+    // Split the expression string into a number, an operator, and another number
+    let [num1, operator, num2] = expression.split(/(\D)/);
+    // Convert the number strings to float numbers
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+
+    // Perform the calculation based on the operator and return the result
+    switch (operator) {
+      case '+': return num1 + num2;
+      case '-': return num1 - num2;
+      case '*': return num1 * num2;
+      case '/': return num1 / num2;
+      default: 
+        // If the operator is unknown, log an error and return 0
+        console.error('Unknown operator', operator); 
+        return 0;
+    }
+  }
 }
 
-function appendOperator(operator) {
-  currentResult += operator;
-  document.getElementById("result").value = currentResult;
-}
-
-function calculateResult() {
-  let result = eval(currentResult);
-  document.getElementById("result").value = result;
-  currentResult = result.toString();
-}
-
-function clearResult() {
-  currentResult = "";
-  document.getElementById("result").value = "";
-}
+// Example usage:
+// Append the number '5'
+calculator.appendNumber('5');
+// Append the operator '+'
+calculator.appendOperator('+');
+// Perform the calculation
+calculator.calculateResult();
+// Clear the result
+calculator.clearResult();
--- 11-dom-manipulation/portfolio-about/port.js
+++ 11-dom-manipulation/portfolio-about/port.js
@@ -1,10 +1,39 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("navbar").style.display = "none";
-  } else {
-    document.getElementById("navbar").style.display = "block";
+// Function to hide/show navbar when scrolling
+function navbarScrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      document.getElementById("navbar").style.display = "none";
+    } else {
+      document.getElementById("navbar").style.display = "block";
+    }
   }
-}
-
+  
+  // Function to show/hide "back to top" button when scrolling
+  function buttonScrollFunction() {
+    let mybutton = document.getElementById("scrollToTop");
+  
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  
+  // Function to scroll back to the top
+  function backToTop() {
+    window.scroll({
+      top: 0, 
+      left: 0, 
+      behavior: 'smooth' 
+    });
+  }
+  
+  // Get the button and add event listener
+  let mybutton = document.getElementById("scrollToTop");
+  mybutton.addEventListener('click', backToTop);
+  
+  // Call both functions when window is scrolled
+  window.onscroll = function() {
+    navbarScrollFunction();
+    buttonScrollFunction();
+  };
+  
