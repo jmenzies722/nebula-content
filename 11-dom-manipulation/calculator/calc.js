b6af110b13f979145a38aa1948c0f0ebd2d2db18
Jmenzies722 <Jmenzies722@gmail.com>
2023-07-29 11:32:05 -0400

    updated

--- 11-dom-manipulation/calculator/calc.js
+++ 11-dom-manipulation/calculator/calc.js
@@ -1,122 +1,172 @@
-const keys = document.querySelectorAll('.key');
-const displayInput = document.querySelector('.display .input');
-const displayOutput = document.querySelector('.display .output');
-const modeButtons = document.querySelectorAll('.mode-btn');
-const multiMode = document.querySelector('.multi-mode');
+document.addEventListener('DOMContentLoaded', () => {
+  const inputDiv = document.querySelector('.input');
+  const outputDiv = document.querySelector('.output');
+  const wordToOperator = {
+      'times': '*',
+      'multiplied by': '*',
+      'plus': '+',
+      'minus': '-',
+      'divided by': '/'
+  };
 
-let input = "";
-let currentMode = 'normal'; // Default mode is normal calculator mode
+  const numberWordsToNumbers = {
+      'zero': 0,
+      'one': 1,
+      'two': 2,
+      'three': 3,
+      'four': 4,
+      'five': 5,
+      'six': 6,
+      'seven': 7,
+      'eight': 8,
+      'nine': 9,
+      'ten': 10
+  };
 
-// Add event listeners to mode buttons
-modeButtons.forEach(btn => {
-  btn.addEventListener('click', () => {
-    currentMode = btn.dataset.mode;
-    displayInput.textContent = `Mode: ${currentMode.toUpperCase()} - Enter the value`;
-    displayOutput.textContent = "";
-    input = "";
-    multiMode.style.display = 'block'; // Show the multi-mode section
+  function clearInput() {
+      inputDiv.innerText = '';
+      outputDiv.innerText = '';
+  }
+
+  function calculate(expression) {
+      let numbers = [];
+      let operators = [];
+      let priority = {
+          "+": 1,
+          "-": 1,
+          "*": 2,
+          "/": 2
+      };
+
+      let segments = expression.match(/(\d+\.?\d*)|[\+\-\*\/]|%/g);
+
+      for (let segment of segments) {
+          if (!isNaN(segment)) {
+              numbers.push(parseFloat(segment));
+          } else {
+              while (
+                  operators.length &&
+                  priority[segment] <= priority[operators[operators.length - 1]]
+              ) {
+                  doMath(numbers, operators);
+              }
+              operators.push(segment);
+          }
+      }
+
+      while (operators.length) {
+          doMath(numbers, operators);
+      }
+
+      return numbers[0];
+  }
+
+  function doMath(numbers, operators) {
+      let num2 = numbers.pop();
+      let num1 = numbers.pop();
+      let op = operators.pop();
+
+      switch (op) {
+          case '+':
+              numbers.push(num1 + num2);
+              break;
+          case '-':
+              numbers.push(num1 - num2);
+              break;
+          case '*':
+              numbers.push(num1 * num2);
+              break;
+          case '/':
+              numbers.push(num1 / num2);
+              break;
+          case '%':
+              numbers.push(num1 / 100);
+              break;
+      }
+  }
+
+  function calculateInput() {
+      let input = inputDiv.innerText;
+      if(input === '') return;
+      try {
+          let output = calculate(input);
+          outputDiv.innerText = output;
+      } catch {
+          outputDiv.innerText = "Error";
+      }
+  }
+
+  document.querySelectorAll('.key').forEach(key => {
+      key.addEventListener('click', e => {
+          const action = e.currentTarget.dataset.key;
+          const text = e.currentTarget.innerText.trim();
+          if(action === 'clear') {
+              clearInput();
+          } else if(action === 'backspace') {
+              inputDiv.innerText = inputDiv.innerText.slice(0, -1);
+          } else if(action === 'voice') {
+              handleVoiceInput();
+          } else if(action === '=') {
+              calculateInput();
+          } else {
+              if (['+', '-', '*', '/'].includes(action)) {
+                  inputDiv.innerText += ` ${action} `;
+              } else {
+                  inputDiv.innerText += ` ${text} `;
+              }
+          }
+      });
   });
-});
 
-// Add event listeners to calculator keys
-keys.forEach(key => {
-  const value = key.dataset.key;
-
-  key.addEventListener('click', () => {
-    if (value === "clear") {
-      input = "";
-      displayInput.textContent = "";
-      displayOutput.textContent = "";
-    } else if (value === "backspace") {
-      input = input.slice(0, -1);
-      displayInput.textContent = CleanInput(input);
-    } else if (value === "=") {
-      let result;
-      if (currentMode === 'tip') {
-        let billAmount = parseFloat(document.getElementById('tip-bill-input').value);
-        let tipPercentage = parseFloat(document.getElementById('tip-percentage-input').value);
-        let tipAmount = (billAmount * tipPercentage) / 100;
-        result = tipAmount;
-      } else {
-        result = eval(PrepareInput(input));
+  document.addEventListener('keydown', e => {
+      const key = e.key;
+      const validKeys = '0123456789.+-*/()%';
+      if(key === 'Enter') {
+          calculateInput();
+      } else if(key === 'Backspace') {
+          inputDiv.innerText = inputDiv.innerText.slice(0, -1);
+      } else if(key === 'Escape') {
+          clearInput();
+      } else if(validKeys.includes(key)) {
+          if (['+', '-', '*', '/'].includes(key)) {
+              inputDiv.innerText += ` ${key} `;
+          } else {
+              inputDiv.innerText += key;
+          }
       }
-      displayOutput.textContent = CleanOutput(result);
-    } else if (value === "brackets") {
-      if (
-        input.indexOf("(") == -1 || 
-        input.indexOf("(") != -1 && 
-        input.indexOf(")") != -1 && 
-        input.lastIndexOf("(") < input.lastIndexOf(")")
-      ) {
-        input += "(";
-      } else if (
-        input.indexOf("(") != -1 && 
-        input.indexOf(")") == -1 || 
-        input.indexOf("(") != -1 &&
-        input.indexOf(")") != -1 &&
-        input.lastIndexOf("(") > input.lastIndexOf(")")
-      ) {
-        input += ")";
-      }
-
-      displayInput.textContent = CleanInput(input);
-    } else {
-      if (ValidateInput(value)) {
-        input += value;
-        displayInput.textContent = CleanInput(input);
-      }
-    }
   });
+
+  function handleVoiceInput() {
+    const recognition = new window.webkitSpeechRecognition();
+    recognition.interimResults = true;
+    recognition.continuous = false;
+    recognition.lang = 'en-US';
+  
+    recognition.onresult = (event) => {
+        for(let i = event.resultIndex; i < event.results.length; ++i) {
+            if(event.results[i].isFinal) {
+                let transcript = event.results[i][0].transcript.toLowerCase();
+                for(let word in wordToOperator) {
+                    const operator = wordToOperator[word];
+                    transcript = transcript.replace(new RegExp(word, 'g'), operator);
+                }
+                // Convert 'x' and 'times' to '*'
+                transcript = transcript.replace(/times/g, '*');
+                transcript = transcript.replace(/x/g, '*');
+  
+                // Convert word numbers to actual numbers
+                for(let word in numberWordsToNumbers) {
+                    const number = numberWordsToNumbers[word];
+                    transcript = transcript.replace(new RegExp("\\b" + word + "\\b", 'g'), number);
+                }
+  
+                inputDiv.innerText = transcript;
+                calculateInput();
+            }
+        }
+    };
+  
+    recognition.start();
+  }
+  
 });
-
-function CleanInput(input) {
-  let input_array = input.split("");
-  let input_array_length = input_array.length;
-
-  for (let i = 0; i < input_array_length; i++) {
-    if (input_array[i] === "*") {
-      input_array[i] = ` × `;
-    } else if (input_array[i] === "/") {
-      input_array[i] = ` ÷ `;
-    } else if (input_array[i] === "%") {
-      input_array[i] = ` % `;
-    }
-  }
-
-  return input_array.join("");
-}
-
-function CleanOutput(output) {
-  return output.toLocaleString();
-}
-
-function ValidateInput(value) {
-  let lastInput = input.slice(-1);
-  let operators = ["+", "-", "*", "/"];
-
-  if (value === "." && lastInput === ".") {
-    return false;
-  }
-
-  if (operators.includes(value)) {
-    if (operators.includes(lastInput)) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-
-  return true;
-}
-
-function PrepareInput(input) {
-  let input_array = input.split("");
-
-  for (let i = 0; i < input_array.length; i++) {
-    if (input_array[i] === "%") {
-      input_array[i] = "/100";
-    }
-  }
-
-  return input_array.join("");
-}
